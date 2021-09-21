68ccf28582edcefa8c069dd342397b1d1b67f932
Mikey J <mikeylashaun@icloud.com>
2021-09-20 23:24:41 -0700

    added some info under profiel card

---
+++ client/src/components/AboutContainer/index.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+const styles = {
+    containerBackground: {
+        backgroundColor: "#FFEDDA",
+        width: "571px",
+        height:"301px",
+        opacity: 0.5,
+        boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+    }
+}
+
+function AboutMe() {
+    return (
+        <Container style={styles.containerBackground} >
+                Description
+                <Row>
+                    <Col style={{fontSize: "14px"}} >
+                    Full-stack Developer in creating and managing websites. Recently earned a Certificate in Full-stack Development from UCLA, expanding skills in HTML, CSS, React, NoSql, MySQL, Bootstrap, and JavaScript. I pride myself on persistence, patience and adaptation to new and challenging environments. During my time at UCLA, I served as a project manager to multiple groups, helping to establish weekly goals as well as strategies and timelines for accomplishing those goals. The freedom of coding and the discipline of utilizing a new competency to make it run efficiently not only excites but motivates me. Passionate about creating life-enhancing user experiences and collaborating with others to develop meaningful mobile and web applications. My professional background in technologies and other mediums, also my drive for problem solving makes me a great piece to add to any organization.
+                    </Col>
+                </Row>
+        </Container>
+    );
+}
+
+export default AboutMe;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,13 +3,14 @@ import React from "react";
 import NavBar from "../components/Navbar";
 import Hero from "../components/Hero Image/inedx"
 import ProfileHead from "../components/Username/index"
+import AboutMe from "../components/AboutContainer/index"
 
 import { Container, Row, Col } from "react-bootstrap";
 
 const styles = {
-    marginTop:{
-        marginTop:"100px", 
-        marginRight:"10px",
+    marginTop: {
+        marginTop: "100px",
+        marginRight: "10px",
     }
 };
 
@@ -30,8 +31,14 @@ function Home() {
                         <Hero />
                     </Col>
                     <Col md={6} >
-                        <ProfileHead/>
-
+                        <Row>
+                            <Col md={12} >
+                                <ProfileHead />
+                            </Col>
+                            <Col>
+                                <AboutMe md={12} />
+                            </Col>
+                        </Row>
                     </Col>
                 </Row>
             </Container>
