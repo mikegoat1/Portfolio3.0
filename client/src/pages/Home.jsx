acdb5342bf5ea728f298a6f04d8df1770adbc9c8
Mikey J <mikeylashaun@icloud.com>
2021-09-21 20:28:51 -0700

    adding skills to profile

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,9 @@ import React from "react";
 import NavBar from "../components/Navbar";
 import Hero from "../components/Hero Image/inedx"
 import ProfileHead from "../components/Username/index"
+import Skills from "../components/SkillsComp/index"
 import AboutMe from "../components/AboutContainer/index"
+
 
 import { Container, Row, Col } from "react-bootstrap";
 
@@ -11,7 +13,11 @@ const styles = {
     marginTop: {
         marginTop: "100px",
         marginRight: "10px",
-    }
+    },
+    ColFlex: {
+        display: "flex",
+    },
+
 };
 
 
@@ -32,8 +38,12 @@ function Home() {
                     </Col>
                     <Col md={6} >
                         <Row>
-                            <Col md={12} >
+                            <Col style={styles.ColFlex} md={7} >
                                 <ProfileHead />
+                                
+                            </Col>
+                            <Col md={5} >
+                                <Skills style={styles.skillsWrap} />
                             </Col>
                             <Col>
                                 <AboutMe md={12} />
