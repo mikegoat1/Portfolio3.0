245b3972f6a36733345b4ec1f504c4e01cded298
Mikey J <mikeylashaun@icloud.com>
2021-10-13 16:43:46 -0700

    further added

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
-import React from "react"; 
-import { BrowserRouter as Router, Route } from "react-router-dom"; 
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx"
 import './App.css';
 
 function App() {
   return (
-   <Router basename={process.env.PUBLIC_URL}>
-    <Route exact path="/" component={Home} /> 
-   </Router>
+    <Router basename={process.env.PUBLIC_URL}>
+      <Route exact path="/" component={Home} />
+    </Router>
   );
 }
 
