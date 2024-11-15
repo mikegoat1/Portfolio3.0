8a4b135a6ef74220365e189d8d547d3ee9139d13
mikegoat1 <mikeg.o.a.t.1@gmail.com>
2024-11-15 14:29:14 -0800

    feat(Index): switch from HashRouter to BrowserRouter for improved routing

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -10,9 +10,9 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
 );
 reportWebVitals();
