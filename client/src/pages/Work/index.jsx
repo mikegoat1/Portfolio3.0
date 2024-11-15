a83994185b5a137f2258c7e06f7e4aac7412138e
mikegoat1 <mikeg.o.a.t.1@gmail.com>
2024-11-14 19:39:48 -0800

    enhance Work component: add project tags, update project details, and improve styling for better presentation

--- client/src/pages/Work/index.jsx
+++ client/src/pages/Work/index.jsx
@@ -1,9 +1,9 @@
 import {
-  Button,
   Card,
   CardActions,
   CardContent,
   CardMedia,
+  Chip,
   Container,
   Grid2,
   IconButton,
@@ -12,15 +12,31 @@ import {
 import React from "react";
 import { makeStyles } from "@mui/styles";
 import GitHubIcon from "@mui/icons-material/GitHub";
-import WebIcon from '@mui/icons-material/Web';
+import WebIcon from "@mui/icons-material/Web";
+import Quiz from "../../assets/Great-Quiz.png";
+import Ticket from "../../assets/ticket-scalper.png";
+import Flicc from "../../assets/FLICCPICKER.png";
+
 // I want to create a landing page first, then have the user navigate to the about me page.
 
 const useStyles = makeStyles({
   root: {
     maxWidth: 345,
+    backgroundColor: "#F4F4F4",
+    boxShadow: "0 4px 8px rgba(138, 143, 153, 0.2)",
+    border: "1px solid #8A8F99",
   },
   media: {
     height: 140,
+  },
+  description: {
+    color: "#8A8F99",
+  },
+  chip: {
+    backgroundColor: "#A8BCA1", // Dusty Sage background color for tags
+    color: "#3C3C3C", // Charcoal color for the text
+    marginRight: 4,
+    marginBottom: 4,
   },
 });
 
@@ -31,21 +47,31 @@ const projects = [
     image: "https://via.placeholder.com/140",
     link: "#",
     gitHub: "#",
+    tags: ["HTML", "CSS", "JavaScript"],
   },
   {
-    title: "Project 2",
-    description: "This is a description of project 2.",
-    image: "https://via.placeholder.com/140",
-    link: "#",
-    gitHub: "#",
-
+    title: "Flick Picker",
+    description: "Ticket-Scalper is a ticket search platform that dynamically aggregates sports, concert, and theater ticket listings from over 60 ticketing sites, offering a seamless and comprehensive browsing experience.",
+    image: Flicc,
+    link: "https://mikegoat1.github.io/FliccPicker/",
+    gitHub: "https://github.com/RandonRussell85/FliccPicker",
+    tags: ["Handlebars", "Sequelize", "CSS3", "Sequelize", "Javascript", "Express-session","DotEnv"],
   },
   {
-    title: "Project 3",
-    description: "This is a description of project 3.",
-    image: "https://via.placeholder.com/140",
-    link: "#",
-    gitHub: "#",
+    title: "Ticket Scalper",
+    description: "Ticket-Scalper is a ticket search platform that dynamically aggregates sports, concert, and theater ticket listings from over 60 ticketing sites, offering a seamless and comprehensive browsing experience.",
+    image: Ticket,
+    link: "https://maple-eh-00291.herokuapp.com",
+    gitHub: "https://github.com/mikegoat1/Ticket-Scalper",
+    tags: ["Handlebars", "Sequelize", "CSS3", "Sequelize", "Javascript", "Express-session","DotEnv"],
+  },
+  {
+    title: "The Great Quiz",
+    description:
+      "A dynamic card component created using a blend of vanilla JavaScript, jQuery, HTML, and CSS, showcasing interactive content and styles for a responsive user experience.",
+    image: Quiz,
+    gitHub: "https://github.com/mikegoat1/The-Great-Quiz",
+    tags: ["Javascript", "jQuery", "HTML5", "CSS3"],
   },
 ];
 const Work = () => {
@@ -65,7 +91,7 @@ const Work = () => {
         sx={{ display: "flex", justifyContent: "center" }}
         gutterBottom
       >
-        My Projects
+        Recent Work
       </Typography>
       <Grid2 container spacing={4} justifyContent="center" alignItems="center">
         {projects.map((project, index) => (
@@ -77,19 +103,43 @@ const Work = () => {
                 title={project.title}
               />
               <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
+                <Typography
+                  gutterBottom
+                  variant="h5"
+                  component="div"
+                  sx={{ color: "#3C3C3C", fontWeight: "bold" }}
+                >
                   {project.title}
                 </Typography>
                 <Typography variant="body2" color="textSecondary" component="p">
                   {project.description}
                 </Typography>
+                <div>
+                  {project.tags.map((tag, index) => (
+                    <Chip
+                      key={index}
+                      label={tag}
+                      className={classes.chip}
+                      size="small"
+                      sx={{ backgroundColor: "#A8BCA1", color: "#3C3C3C" }}
+                    />
+                  ))}
+                </div>
               </CardContent>
               <CardActions>
-                <IconButton href={project.gitHub} aria-label="GitHub Link">
-                    <GitHubIcon />
+                <IconButton
+                  href={project.gitHub}
+                  sx={{ color: "#E2725B" }}
+                  aria-label="GitHub Link"
+                >
+                  <GitHubIcon />
                 </IconButton>
-                <IconButton href={project.link} aria-label="Host Link">
-                    <WebIcon />
+                <IconButton
+                  href={project.link}
+                  sx={{ color: "#E2725B" }}
+                  aria-label="Host Link"
+                >
+                  <WebIcon />
                 </IconButton>
               </CardActions>
             </Card>
